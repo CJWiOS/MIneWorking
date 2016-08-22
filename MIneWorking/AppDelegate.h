c54becbc77e46c1d144fc970386347c55e9de14c
你的名字good-boy <18236931209@163.com>
2016-08-23 00:01:53 +0800

    是电风扇的
    大的

---
+++ MIneWorking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MIneWorking
+//
+//  Created by 我赢职场 on 16/8/22.
+//  Copyright © 2016年 我赢职场. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MIneWorking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MIneWorking
+//
+//  Created by 我赢职场 on 16/8/22.
+//  Copyright © 2016年 我赢职场. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
